d2f0eabd01d6a6cce55de7779328ee7803f9827d
zhoufa <zhoufa163537@163.com>
2016-09-02 15:20:47 +0800

    没用到，无用Analyzer

--- src/main/java/com/zhoufa/lucene/analyzer/PositionPorterStopAnalyzer.java
+++ src/main/java/com/zhoufa/lucene/analyzer/PositionPorterStopAnalyzer.java
@@ -14,6 +14,7 @@ import java.io.IOException;
  * @author zhou.fa@diligentfirst.com
  * @create 2016-09-02-11
  */
+@Deprecated
 public class PositionPorterStopAnalyzer extends Analyzer {
 
     private CharArraySet stopWords;
